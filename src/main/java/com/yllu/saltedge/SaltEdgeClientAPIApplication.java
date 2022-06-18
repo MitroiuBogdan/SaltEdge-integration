6fc24ab1a4fadb762e1b8d0c672a24abe140e25b
bogdan.mitroiu <bogdan.mitroiu@stud.etti.upb.ro>
2022-06-19 00:02:43 +0300

    externalise properties

--- src/main/java/com/yllu/saltedge/SaltEdgeClientAPIApplication.java
+++ src/main/java/com/yllu/saltedge/SaltEdgeClientAPIApplication.java
@@ -1,12 +1,17 @@
 package com.yllu.saltedge;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 
 @SpringBootApplication
 public class SaltEdgeClientAPIApplication implements CommandLineRunner {
 
+
+    @Value("${spring.datasource.url}")
+    String jdbcURL;
 
     public static void main(String[] args) {
         SpringApplication.run(SaltEdgeClientAPIApplication.class, args);
@@ -14,6 +19,6 @@ public class SaltEdgeClientAPIApplication implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
-
+        System.out.println(jdbcURL);
     }
 }
