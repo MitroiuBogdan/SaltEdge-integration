77610d6e9226b64fc42b2370e983361be4228c8d
bogdan.mitroiu <bogdan.mitroiu@stud.etti.upb.ro>
2022-06-19 00:46:20 +0300

    add refresh scope

--- src/main/java/com/yllu/saltedge/SaltEdgeClientAPIApplication.java
+++ src/main/java/com/yllu/saltedge/SaltEdgeClientAPIApplication.java
@@ -1,24 +1,12 @@
 package com.yllu.saltedge;
 
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.context.config.annotation.RefreshScope;
 
 @SpringBootApplication
-public class SaltEdgeClientAPIApplication implements CommandLineRunner {
-
-
-    @Value("${spring.datasource.url}")
-    String jdbcURL;
+public class SaltEdgeClientAPIApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SaltEdgeClientAPIApplication.class, args);
-    }
-
-    @Override
-    public void run(String... args) throws Exception {
-        System.out.println(jdbcURL);
     }
 }
